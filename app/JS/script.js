fd06c21529e74528dbfd86f0d21d68c28df9e61a
Daniel-Dukundane <dukundanedanny1@gmail.com>
2021-04-20 20:50:34 +0200

    finished mobile menu

--- app/JS/script.js
+++ app/JS/script.js
@@ -1,1 +1,33 @@
-console.log("Hello world");
+
+const btnHamburger = document.querySelector("#btnhamburger");
+const header = document.querySelector(".header");
+const body = document.querySelector("body");
+const overlay = document.querySelector(".overlay")
+const  fadeelems = document.querySelectorAll(".has-fade")
+btnHamburger.addEventListener('click',function(){
+
+if(header.classList.contains("open")){
+    header.classList.remove('open')
+    fadeelems.forEach(function (element) {
+        element.classList.remove("fade-in")
+        element.classList.add("fade-out") 
+    })
+    body.classList.remove('noScroll')
+
+    
+}
+else{
+    
+    header.classList.add('open')
+    fadeelems.forEach(function (element) {
+        element.classList.remove("fade-out")
+        element.classList.add("fade-in")
+    })
+    body.classList.add('noScroll')
+
+    
+}
+
+
+
+})
